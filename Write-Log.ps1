642c9f5336877d712d9867f472ad80d0109a100f
rbconard <robert.conard@gmail.com>
2014-10-14 11:20:35 -0500

    Write-Log
    Function that writes to log file with time-date stamp, and to screen if
    -verbose flag is used.

---
+++ Write-Log.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.Synopsis
+   Writes text to a log file with time date stamp on each line
+.DESCRIPTION
+   Writes text to a log file with time date stamp on each line.   If the user
+   uses the -verbose flag it will write to the screen as well
+.EXAMPLE
+   Write-Log "Hello World" C:\Temp\HelloWorld.Log
+
+   Writes "Hello World" to log file on C:\Temp\HelloWorld.log
+#>
+#- Function to write to log file and display to screen
+function Write-Log {
+    [CmdLetBinding()]
+	Param(
+        # Message to write to log file
+        [String]$MessageOut, 
+        
+        # Logfile to write
+        [ValidateScript({Test-Path $_})]
+        [String]$LogFileOut
+    )
+
+    $Messageout = ("$(Get-Date -Format "yyyy/MM/dd hh:mm:ss.fff") $MessageOut")
+	Write-Verbose $MessageOut
+	$MessageOut | Out-File $LogFileOut -Append
+}
